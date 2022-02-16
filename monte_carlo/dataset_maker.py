5abdf8dd49ad009ab0533f4d740da3926a4822ee
Alexander Yaskevich <yaskevich.san@mail.ru>
2022-02-16 19:59:22 +0300

    Remove ttm parameter (can be expressed in terms of volatility)

--- monte_carlo/dataset_maker.py
+++ monte_carlo/dataset_maker.py
@@ -13,9 +13,8 @@ def create_dataset(entries_cnt: int) -> pd.DataFrame:
     rng = default_rng(OPTIONS_PARAMS_RANDOM_SEED)
     data = {
         'spot_strike_ratio': 0.5 + rng.random(entries_cnt) * 1.5,
-        'ttm': 0.5 + rng.random(entries_cnt) * 1,
-        'risk_free_rate': 0 + rng.random(entries_cnt) * 0.1,
-        'volatility': 0.05 + rng.random(entries_cnt) * 0.5,
+        'risk_free_rate': 0 + rng.random(entries_cnt) * 0.2,
+        'volatility': 0.05 + rng.random(entries_cnt) * 0.95,
         'avg_type': [OptionAvgType.ARITHMETIC.value if rng.random() >= 0.5 else OptionAvgType.GEOMETRIC.value
                      for _ in range(entries_cnt)],
     }
@@ -26,6 +25,6 @@ def create_dataset(entries_cnt: int) -> pd.DataFrame:
 
 
 def _get_price(row) -> Tuple[float, float, float]:
-    paths = generate_paths(row.ttm, row.risk_free_rate, row.volatility, row.spot_strike_ratio)
-    return get_option_price_and_ci(paths, row.risk_free_rate, row.ttm,
+    paths = generate_paths(row.risk_free_rate, row.volatility, row.spot_strike_ratio)
+    return get_option_price_and_ci(paths, row.risk_free_rate,
                                    OptionAvgType(row.avg_type))
--- monte_carlo/pricer.py
+++ monte_carlo/pricer.py
@@ -3,13 +3,12 @@ from typing import Tuple
 import numpy as np
 from scipy.stats import norm
 
-from settings import USE_ANTITHETIC_VARIATES, NUMBER_OF_PATHS, CONFIDENCE_INTERVAL
+from settings import USE_ANTITHETIC_VARIATES, NUMBER_OF_PATHS, CONFIDENCE_INTERVAL, TIME_TO_MATURITY
 from utils.typing import OptionAvgType
 
 
 def get_option_price_and_ci(paths: np.ndarray,
                             risk_free_rate: float,
-                            ttm: float,
                             avg_type: OptionAvgType
                             ) -> Tuple[float, float, float]:
     if avg_type == OptionAvgType.ARITHMETIC:
@@ -25,8 +24,8 @@ def get_option_price_and_ci(paths: np.ndarray,
     if USE_ANTITHETIC_VARIATES:
         payoffs = (payoffs[:NUMBER_OF_PATHS] + payoffs[NUMBER_OF_PATHS:]) / 2
 
-    payoff_mean = np.exp(-risk_free_rate * ttm) * payoffs.mean()
-    payoff_std = np.exp(-risk_free_rate * ttm) * payoffs.std()
+    payoff_mean = np.exp(-risk_free_rate * TIME_TO_MATURITY) * payoffs.mean()
+    payoff_std = np.exp(-risk_free_rate * TIME_TO_MATURITY) * payoffs.std()
     ci_multiplier = norm.ppf((1 + CONFIDENCE_INTERVAL) / 2)
     return (payoff_mean,
             max(payoff_mean - payoff_std * ci_multiplier / np.sqrt(NUMBER_OF_PATHS), 0),
