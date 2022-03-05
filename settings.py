0cf0fbf90a57bf2b4f35e61ff75c41e5d303d4cf
Alexander Yaskevich <yaskevich.san@mail.ru>
2022-03-05 17:50:23 +0300

    Experiments with parameters

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 from utils.typing import OptionAvgType
 
 # Launch Monte Carlo or not
-USE_DATA_FROM_FILE = True
+USE_DATA_FROM_FILE = False
 
 # Monte Carlo parameters
 NUMBER_OF_PATHS = 100
@@ -10,14 +10,14 @@ CONFIDENCE_INTERVAL = 0.95
 USE_ANTITHETIC_VARIATES = True
 
 # Random number generator seeds
-PATH_RANDOM_SEED = None  # Set None not to fix seed
-OPTIONS_PARAMS_RANDOM_SEED = None  # Set None not to fix seed
+PATH_RANDOM_SEED = 42  # Set None not to fix seed
+OPTIONS_PARAMS_RANDOM_SEED = 234  # Set None not to fix seed
 
 # Neural networks
 DATASET_SIZE = 100000  # Used only when USE_DATA_FROM_FILE = False
-EPOCHS_COUNT = 500
+EPOCHS_COUNT = 150
 BATCH_SIZE = 100
-USE_CONVEX_NETWORK = True
+USE_CONVEX_NETWORK = False
 
 # General
 VERBOSE = True
